d20e2428fd501ed2f9e65542f12037d177da75b3
Rohith chand <154948070+Werohith45@users.noreply.github.com>
2026-07-07 12:51:27 +0530

    Add program to find the biggest of three numbers

---
+++ Biggest of Three.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int m1,m2,m3;
+ cout <<"Enter the First Number:";
+ cin>> m1;
+  cout <<"Enter the Second Number:";
+  cin>> m2;
+   cout <<"Enter the Third Number:";
+   cin>>m3;
+   if (m1>=m2)
+   cout<<"The Greast Number is: "<< m1;
+    else if (m2>=m3)
+    cout<<"The Greast Number is: "<<m2;
+        else 
+  cout<<"The Greast Number is: "<< m3;
+
+ }
+
