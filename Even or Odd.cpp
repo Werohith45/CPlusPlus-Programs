c5688559d221e74607eeba9099de2f330274a807
Rohith chand <154948070+Werohith45@users.noreply.github.com>
2026-07-07 12:36:58 +0530

    Add Even or Odd number checker program

---
+++ Even or Odd.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int m1,m2;
+ cout<<"Enter the First number:";
+ cin>> m1;
+ cout<<"Enter the second number:";
+ cin>> m2;
+
+ // Checking First number 
+ if (m1%2==0)
+ cout<<"\n The First number is Even:"<< m1;
+    else 
+ cout<<"\n The First number is Odd:"<< m1;
+
+ // checking Second number 
+     if (m2%2==0)
+ cout<<"\n And the Second number is Even:"<< m2;
+           
+     else 
+ cout<<"\n And the Secomd number is Odd:"<< m2;
+
+}
