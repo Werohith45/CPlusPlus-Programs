9357f942a8040ae24b6290dfd8aeed745438a1af
Rohith chand <154948070+Werohith45@users.noreply.github.com>
2026-07-07 13:58:56 +0530

    Implement multiplication table generation
    Fix the for loop initialization to use '=' instead of '==' for correct iteration.

---
+++ Multiplication Table.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int num,i;
+ cout <<"Enter the Number To Get Table";
+ cin>>num;
+ cout << "\nMultiplication Table for " << num << ":\n";
+
+ for (i==1;i<=10;i++)
+ 
+ cout<<num<<"X"<<i<<"="<<(num*i);
+ 
+
+}
