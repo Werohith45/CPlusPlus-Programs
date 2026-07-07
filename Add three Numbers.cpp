43b28e4cc8cde16a530bff9eab55b60ca8785d12
Rohith chand <154948070+Werohith45@users.noreply.github.com>
2026-07-07 11:41:50 +0530

    Add program to sum three numbers

---
+++ Add three Numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int m1, m2, m3, sum;
+cout << "\n Enter Mark 1: ";
+cin >> m1;
+cout << "\n Enter Mark 2: ";
+cin >> m2;
+cout << "\n Enter Mark 3: ";
+cin >> m3;
+sum = m1 + m2 + m3;
+cout << "\n The sum = " << sum;
+}
